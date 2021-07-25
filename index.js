5f1326775651050ba97fcfc7db973376cdaa6bf1
Charles Lobe <lobecf@gmail.com>
2021-07-25 12:26:23 -0400

    commit tree ba48cd3ad71e6133cce205d81bc7c3d2e15692a3

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function fuzzyMatch(drivers, name){
+    return drivers.filter(driver => driver.substring(0,name.length) === name)
+}
+
+function findMatching(drivers, name){
+    return drivers.filter(el => el.toLowerCase() === name.toLowerCase());
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function(hometown){
+      return hometown.name === name
+    })
+  }
